92c970bfa7d6135b82b5ab3413ffc1822b5b27e8
양진호/서비스개발1팀/NF <ddukda@nhnfashiongo.com>
2022-04-11 23:25:17 +0900

    1

---
+++ src/main/java/hello/servlet/web/MemberFormServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.web;
+
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+
+        res.setContentType("text/html");
+        res.setCharacterEncoding("utf-8");
+        PrintWriter w = res.getWriter();
+        
+    }
+}
